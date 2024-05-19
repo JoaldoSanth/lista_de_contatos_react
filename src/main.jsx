fa424bb116a0f915e56f3c176426b2fee05d2b1e
JoaldoSanth <joaldosant3@gmail.com>
2024-05-19 02:33:26 -0300

    projeto de react

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Lista_de_contatos from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Lista_de_contatos />
+  </React.StrictMode>,
+)
